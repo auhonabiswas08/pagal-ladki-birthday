bcea4dbed31e650d893e3e50b08b5a7110ce5a30
auhonabiswas08 <auhonab22@iitk.ac.in>
2026-04-11 12:37:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ spread:180
 });
 
 }
+function openLetter(id){
+document.getElementById(id).style.display="block";
+}
+
+function closeLetter(id){
+document.getElementById(id).style.display="none";
+}
+
+const affirmations=[
+"The way your mind works is genuinely rare.",
+"You are effortlessly pretty.",
+"I hate it when you underestimate yourself, please don’t do it and even if you do I am always there to remind you otherwise.",
+"The way you care is something I’ll admire forever."
+];
+
+function showAffirmation(index){
+document.getElementById("affirmationText").innerText=affirmations[index-1];
+}
